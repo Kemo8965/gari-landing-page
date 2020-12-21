8c964bcdad2e6853f05a33b71a754fbba13409a3
Daniel SItali <danielmsitali@outlook.com>
2020-12-21 10:48:23 +0200

    adding placeholder text

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App: React.FC = () => {
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h3>Gari Motor Insurance.</h3>
+            <p>The Next Generation Motor Insurance Platform.</p>
+        </div>
+    );
+};
 
 export default App;
