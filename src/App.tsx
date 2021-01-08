fef670e511207a6858138c1c5ac5ea3d0cb3215a
Daniel SItali <danielmsitali@outlook.com>
2021-01-08 16:27:22 +0200

    added tailwind dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const App: React.FC = () => {
     return (
         <div style={{ textAlign: "center", padding: 28 }}>
-            <h1>Gari Motor Insurance.</h1>
+            <h1>Zambia's First Online Motor Insurance</h1>
             <p>The Next Generation Motor Insurance Platform.</p>
             <h3>Coming Soon</h3>
         </div>
